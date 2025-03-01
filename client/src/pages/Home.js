02adc30fcad28e312768141a6527a33d6ab3dff4
zad Babaei <zadbabaei@gmail.com>
2025-03-01 14:33:49 -0330

    changing the home page design

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,56 +3,19 @@ import { motion } from "framer-motion";
 import { PlayIcon, FilmIcon } from "@heroicons/react/24/solid"; // ✅ Correct import for Heroicons v2
 import axios from "axios";
 import "./Home.css";
-import movies from "../data/movies.js";
+import AnimatedMovie from "../component/AnimatedMovie";
+import HeroNavbar from "../component/HeroNavbar";
 
-const randomPositions = [
-  { top: "50%", left: "5%" },
-  { top: "65%", left: "7%" },
-  { top: "70%", left: "1%" },
-  { top: "60%", left: "9%" },
-  { top: "70%", left: "11%" },
-  { top: "60%", left: "13%" },
-  { top: "70%", left: "15%" },
-  { top: "70%", left: "20%" },
-  { top: "55%", left: "17%" },
-  { top: "70%", left: "19%" },
-  { top: "70%", left: "22%" },
-  { top: "70%", left: "27%" },
-  { top: "70%", left: "30%" },
-  { top: "70%", left: "30%" },
-  { top: "70%", left: "30%" },
-  { top: "70%", left: "30%" },
-  { top: "70%", left: "30%" },
-  { top: "70%", left: "37%" },
-  { top: "70%", left: "33%" },
-  { top: "60%", left: "41%" },
-  { top: "60%", left: "45%" },
-  { top: "66%", left: "47%" },
-  { top: "67%", left: "53%" },
-  { top: "60%", left: "57%" },
-  { top: "50%", right: "5%" },
-  { top: "65%", right: "7%" },
-  { top: "58%", right: "1%" },
-  { top: "60%", right: "9%" },
-  { top: "65%", right: "11%" },
-  { top: "60%", right: "13%" },
-  { top: "55%", right: "15%" },
-  { top: "50%", right: "20%" },
-  { top: "55%", right: "17%" },
-  { top: "70%", right: "19%" },
-  { top: "70%", right: "22%" },
-  { top: "70%", right: "27%" },
-  { top: "70%", right: "30%" }
-  
-];
+
+
+
 
 
 function Home() {
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
   const [currentMovieIndex, setCurrentMovieIndex] = useState(0);
-  const [position, setPosition] = useState(randomPositions[0]);
-
+ 
 
   useEffect(() => {
     const fetchUserData = async () => {
@@ -80,54 +43,12 @@ function Home() {
 
 
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCurrentMovieIndex((prevIndex) => (prevIndex + 1) % movies.length);
-      setPosition(randomPositions[Math.floor(Math.random() * randomPositions.length)]);
-    }, 700); // Change movie every 3 seconds
-
-    return () => clearInterval(interval);
-  }, []);
-
   return (
     
     <div className="home-container">
 
-      {/* Hero Section */}
-      <motion.div
-        className="hero-banner"
-        initial={{ opacity: 0, y: -50 }}
-        whileInView={{ opacity: 1, y: 0 }}
-        exit={{ opacity: 0, y: -50 }}
-        viewport={{ once: false, amount: 0.5 }}
-        transition={{ duration: 3 }}
-      >
+     <HeroNavbar />
 
-      <img
-        src="https://image.tmdb.org/t/p/original/7ucaMpXAmlIM24qZZ8uI9hCY0hm.jpg"
-        alt="Banner"
-        className="banner-image"
-      />
-
-        <div className="banner-overlay" />
-        <motion.h1
-          className="banner-text"
-          initial={{ opacity: 0, scale: 0.8 }}
-          animate={{ opacity: 1, scale: 1 }}
-          transition={{ duration: 0.8 }}
-        >
-          WATCH, TRACK, AND RELIVE THE FUN!
-        </motion.h1>
-      </motion.div>
-
-      {/* Navbar */}
-      <div className="glassy-navbar">
-        <span className="nav-left">Welcome, {user ? user.name : "Guest"}!</span>
-        <div className="nav-right">
-          <a href="#">Create Movie Group</a>
-          <a href="#">Schedule Watch Session</a>
-        </div>
-      </div>
 
 {/* Action Buttons */}
 <motion.div
@@ -137,7 +58,7 @@ function Home() {
   transition={{ duration: 1 }}
 >
   <button>
-    <PlayIcon className="button-icon" /> Your Movie Groups
+    <PlayIcon className="button-icon" />  Movie Groups
   </button>
   <button>
     <FilmIcon className="button-icon" /> Upcoming Movies
@@ -149,17 +70,7 @@ function Home() {
 
 
   {/* Animated Movie Poster */}
-      <motion.div
-        key={currentMovieIndex}
-        className="movie-poster"
-        style={position}
-        initial={{ opacity: 0, scale: 0.8 }}
-        animate={{ opacity: 1, scale: 1 }}
-        exit={{ opacity: 0, scale: 0.5 }}
-        transition={{ duration: 1 }}
-      >
-        <img src={movies[currentMovieIndex].image} alt={movies[currentMovieIndex].title} />
-      </motion.div>
+  <AnimatedMovie />
     </div>
   );
 }
