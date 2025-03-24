dce6186ba7a5feed127fe71caf825d97c7df410e
zad Babaei <zadbabaei@gmail.com>
2025-03-24 13:48:24 -0230

    refactoring the group page and some more...

--- client/src/pages/GroupPage.js
+++ client/src/pages/GroupPage.js
@@ -70,22 +70,25 @@ const handleDeleteMovie = async (movieId) => {
       />
       <div className="group-content">
         {/* Members */}
+        <h1 className="group-members-title">Group Members</h1>
         <div className="group-members">
-          <h1 className="group-members-title">Group Members</h1>
           <div className="group-member-card-container">
-            {group.members.map((member) => (
-              <div key={member._id} className="group-members-card">
-                <img
-                  src={member.profilePic || "https://i.pravatar.cc/100"}
-                  alt={member.name}
-                />
-                <div className="divider"></div>
-                <h2>{member.name}</h2>
-              </div>
-            ))}
+            <div className="group-member-cards-wrapper">
+              {group.members.map((member) => (
+                <div key={member._id} className="group-members-card">
+                  <img
+                    src={member.profilePic || "https://i.pravatar.cc/100"}
+                    alt={member.name}
+                  />
+                  <div className="divider"></div>
+                  <h2>{member.name}</h2>
+                </div>
+              ))}
+            </div>
           </div>
-          <button className="group-member-btn">Invite Friends</button>
         </div>
+
+        <button className="group-member-btn">Invite Friends</button>
 
         {/* Modular SearchBar */}
         <SearchBar onMovieSelect={handleMovieAdd} />
