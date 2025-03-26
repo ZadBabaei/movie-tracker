ee6daddcdc548512ed6af45ff1e4cee0b09678e8
zad Babaei <zadbabaei@gmail.com>
2025-03-26 14:02:39 -0230

    fixed the invite btn and create a group modal page...

--- server/models/Groups.js
+++ server/models/Groups.js
@@ -2,10 +2,14 @@ const mongoose = require("mongoose");
 
 const groupSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  creator: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  creator: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
   members: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
-  movies: [{ type: mongoose.Schema.Types.ObjectId, ref: "Movie" }] // ✅ Ensure This Exists
+  pendingInvitations: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }], 
+  movies: [{ type: mongoose.Schema.Types.ObjectId, ref: "Movie" }],
 });
 
-// ✅ Corrected Export
 module.exports = mongoose.models.Group || mongoose.model("Group", groupSchema);
--- server/models/movie.js
+++ server/models/movie.js
@@ -4,7 +4,8 @@ const MovieSchema = new mongoose.Schema({
   title: { type: String, required: true },
   imdbID: { type: String, unique: true, required: true },
   poster: { type: String },
-  addedBy: { type: mongoose.Schema.Types.ObjectId, ref: "User" }
+  vote_average: { type: Number, default: 0 },
+  addedBy: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
 });
 
 module.exports = mongoose.model("Movie", MovieSchema);
